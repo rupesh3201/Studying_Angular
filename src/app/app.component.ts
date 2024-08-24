7c5de5a9c0c78a9ff6380afb87a2d37532ed4f5b
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-25 00:50:33 +0530

    /type any  which allows all type of data   val

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
-  // making variable
+  // making variable making variable in  type script by 2 ways 1st way num:nuber =20 and secod num=20
+
+  num: number = 20
+  value = 20
+  //two ways to write varible in type script 
+  name : string = "Rupesh" 
+  city = "pune"
+  
 }
