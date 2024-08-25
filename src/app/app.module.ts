d33899299c462b575dc06d5426b1ae96c15efd4d
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-26 00:05:37 +0530

    created component second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
+    SecondComponent,
  
   ],
   imports: [
